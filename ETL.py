5733c8a173902a014c5db649bdc00efbcdfdae1c
min01211 <u1462862@utah.edu>
2025-08-14 21:47:17 -0600

    drop duplicates and n/a

--- ETL.py
+++ ETL.py
@@ -6,7 +6,7 @@ from urllib.parse import quote_plus
 
 ####Extract#####
 #Setting Datasets Path / DB Setting
-Base = "C:/Users/dlwhd/OneDrive/바탕 화면/Projects/ETL Project"
+Base = "C:/Users/dlwhd/OneDrive/바탕 화면/Projects/ETL Ecommerce Project"
 Raw = os.path.join(Base, "datasets", "raw")
 Processed = os.path.join(Base, "datasets", "processed")
 
@@ -15,7 +15,7 @@ Password = quote_plus("Happydk@1")
 engine = create_engine("mysql+mysqlconnector://root:Password@localhost/ecommerce_db")
 
 #Read CSV Files
-order = pd.read_csv(os.path.join(Raw, "olist_orders_dataset.csv"), 
+orders = pd.read_csv(os.path.join(Raw, "olist_orders_dataset.csv"), 
 parse_dates = ["order_purchase_timestamp",
         "order_approved_at",
         "order_delivered_carrier_date",
@@ -46,7 +46,7 @@ translations = pd.read_csv(os.path.join(Raw, "product_category_name_translation.
 dtype = {"product_category_name":"string",
          "product_category_name_english":"category"})   
 
-selllers = pd.read_csv(os.path.join(Raw, "olist_sellers_dataset.csv"),
+sellers = pd.read_csv(os.path.join(Raw, "olist_sellers_dataset.csv"),
 dtype = {"seller_id":"string",
          "seller_city":"category",
          "seller_state":"category"})
@@ -54,6 +54,16 @@ dtype = {"seller_id":"string",
 geolocations = pd.read_csv(os.path.join(Raw, "olist_geolocation_dataset.csv"),
 dtype = {"geolocation_city":"category",
          "geolocation_state":"category"})
+
+#drop duplicates, drop n/a
+orders = orders.drop_duplicates(), orders.dropna()
+items = items.drop_duplicates(), items.dropna()
+payments = payments.drop_duplicates(), payments.dropna()
+customers = customers.drop_duplicates(), customers.dropna()
+products = products.drop_duplicates(), products.dropna()
+translations = translations.drop_duplicates(), translations.dropna()
+sellers = sellers.drop_duplicates(), sellers.dropna()
+geolocations = geolocations.drop_duplicates(), geolocations.dropna()
 
 ####Transform#####
 
